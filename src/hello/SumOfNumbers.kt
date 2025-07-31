ead9b4a0cd5b4f104565bcb89a9ca6387c7529c9
Vladislav Samura <vx5080@yandex.ru>
2025-07-31 23:57:50 +0900

    add task

--- src/hello/SumOfNumbers.kt
+++ src/hello/SumOfNumbers.kt
@@ -2,7 +2,7 @@ package hello
 
 fun main() {
     val numbers = readln().split(" ")
-    var result = 0
+    var result: Int = 0
     for (number in numbers){
         result+= number.toInt()
     }
